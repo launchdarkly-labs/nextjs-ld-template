bb5a02fcb32338f3c1bfc457b96ffe77e2ee64df
Cody De Arkland <codydearkland@gmail.com>
2024-05-19 23:45:19 -0700

    bumping next version, new ld provider memo config

--- components/ldprovider.tsx
+++ components/ldprovider.tsx
@@ -1,24 +1,25 @@
 "use client";
 
-import { use } from "react";
-import { asyncWithLDProvider } from "launchdarkly-react-client-sdk";
+import { use, useMemo } from "react";
+import { LDContext, asyncWithLDProvider } from "launchdarkly-react-client-sdk";
 
 export default function AsyncLDProvider({
   children,
-  context
+  context,
 }: {
   children: React.ReactNode;
-  context: any
+  context: LDContext;
 }) {
-  const LDDynaProvider = use(
+  const ld = useMemo(() => 
     asyncWithLDProvider({
       clientSideID: process.env.NEXT_PUBLIC_LD_CLIENT_SDK_KEY || '',
       reactOptions: {
         useCamelCaseFlagKeys: false
       },
       context: context
-    })
-  );
+    }), [])
+
+  const LDDynaProvider = use(ld);
 
   return <LDDynaProvider>{children}</LDDynaProvider>;
 }
