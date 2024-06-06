f8c671a5ab8fa1a670f5264c6c94cbda95ae8f3e
Yusinto Ngadiman <yusinto@gmail.com>
2024-06-06 16:47:07 -0700

    fix: Added useMemo to remove react warning.

--- launchDarkly/client/LDProvider.tsx
+++ launchDarkly/client/LDProvider.tsx
@@ -3,7 +3,7 @@
 import { LDOptions } from 'launchdarkly-js-client-sdk';
 import { LDContext } from 'launchdarkly-js-sdk-common';
 import { asyncWithLDProvider } from 'launchdarkly-react-client-sdk';
-import { ReactNode, use } from 'react';
+import { ReactNode, use, useMemo } from 'react';
 
 export function LDProvider({
   children,
@@ -14,14 +14,18 @@ export function LDProvider({
   context: LDContext;
   options: LDOptions;
 }) {
-  const Provider = use(
-    asyncWithLDProvider({
-      clientSideID: process.env.NEXT_PUBLIC_LD_CLIENT_SDK_KEY ?? '',
-      context,
-      options,
-      timeout: 5,
-    }),
+  const Provider = useMemo(
+    () =>
+      asyncWithLDProvider({
+        clientSideID: process.env.NEXT_PUBLIC_LD_CLIENT_SDK_KEY ?? '',
+        context,
+        options,
+        timeout: 5,
+      }),
+    [context, options],
   );
 
-  return <Provider>{children}</Provider>;
+  const LDClientProvider = use(Provider);
+
+  return <LDClientProvider>{children}</LDClientProvider>;
 }
